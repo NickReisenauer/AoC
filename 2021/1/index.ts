8cf34d72af412ba9f3a63536476f6ada02e3c3f9
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2022-02-09 21:12:27 +0000

    feat: map input from txt

--- 2021/1/index.ts
+++ 2021/1/index.ts
@@ -1,5 +1,7 @@
-import * as fs from "fs";
+// @ts-ignore - Just complaining about types
+import fs from "fs";
 
-const input = fs.readFileSync("./input.txt", "utf8");
+const inputFile = fs.readFileSync("./input.txt", "utf8");
+const input = inputFile.split("\n").map((line: string) => Number(line));
 
-console.log(input)
+// 1154
