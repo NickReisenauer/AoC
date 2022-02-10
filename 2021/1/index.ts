e15a19e52a16590652c65b22b01be566e7a81f37
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2022-02-10 14:28:34 +0000

    feat: ✅ 2021/1

--- 2021/1/index.ts
+++ 2021/1/index.ts
@@ -1,7 +1,18 @@
 // @ts-ignore - Just complaining about types
 import fs from "fs";
 
-const inputFile = fs.readFileSync("./input.txt", "utf8");
-const input = inputFile.split("\n").map((line: string) => Number(line));
+const input = fs
+  .readFileSync("./input.txt", "utf8")
+  .trim()
+  .split("\n")
+  .map((line: string) => Number(line));
 
-// 1154
+let count = 0;
+for (let i = 0; i < input.length; i += 1) {
+  const current = input[i];
+  const previous = input[i - 1];
+
+  if (current > previous) count += 1;
+}
+
+console.log(count);
