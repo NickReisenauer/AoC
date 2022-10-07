0837c068982b0d75e03312f15c6ee24f6615a81f
Nick Reisenauer <nickdreisenauer@gmail.com>
2022-10-07 12:38:50 -0700

    feat: part 2 of day 1 in Rust

--- 2021/1/Rust/src/main.rs
+++ 2021/1/Rust/src/main.rs
@@ -17,5 +17,31 @@ fn main() {
         }
     }
 
-    println!("Count: {count}");
+    println!("Part 1 Count: {count}");
+    part_two()
+}
+
+fn part_two() {
+    // let test_input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let input =
+        fs::read_to_string("./input.txt").expect("Something went wrong reading the input file");
+    let numbers: Vec<u16> = input.split('\n').map(|x| x.parse().unwrap()).collect();
+
+    let mut count = 0;
+
+    for (i, _num) in numbers.iter().enumerate() {
+        if i < numbers.len() - 3 {
+            let window = [numbers[i], numbers[i + 1], numbers[i + 2]];
+            let next_window = [numbers[i + 1], numbers[i + 2], numbers[i + 3]];
+
+            let window_sum = window.iter().fold(0, |acc, cur| acc + cur);
+            let next_window_sum = next_window.iter().fold(0, |acc, cur| acc + cur);
+
+            if next_window_sum > window_sum {
+                count += 1;
+            }
+        }
+    }
+
+    println!("Part 2 Count: {count}")
 }
